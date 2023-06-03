9fa63861a0ee5082247f313cd363d088d7803ded
Yonggyu <whdydrb741@gmail.com>
2023-06-04 02:57:18 +0900

    load

---
+++ _tests_/hello.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest')
+const {app, server} = require('../app.js')
+
+describe('app', function(){
+    afterAll(()=> server.close())
+
+    test('hello api', (done) => {
+        request(app)
+            .get('/hello')
+            .expect(200)
+            .end((err, res) =>{
+                if(err) throw err
+                res.body.toMatchObject({
+                    result : true,
+                    message : 'hello, world!'
+                })
+                done()
+
+            })
+            
+    })
+    
+})
